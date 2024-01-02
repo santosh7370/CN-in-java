03251f091523f2b0adb058a5ad631d11fda499ca
Santosh Singhaniya <116973860+santosh7370@users.noreply.github.com>
2024-01-02 21:37:38 +0530

    Add files via upload

---
+++ ASCE.java
@@ -0,0 +1,13 @@
+class ASCE {
+    public static void main(String agr[])
+    {
+       // int a=8;
+        for(int i=0;i<=10;i++){
+            for(int j=1;j<=100;j++){
+                System.out.print(i+j +"\t");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
